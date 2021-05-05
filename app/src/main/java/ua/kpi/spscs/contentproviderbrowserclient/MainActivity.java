e29763d20b932a399e8d60067a975f42dd9e6e44
Aroxed <petrashenko@gmail.com>
2021-05-05 22:40:52 +0300

    Initial commit

---
+++ app/src/main/java/ua/kpi/spscs/contentproviderbrowserclient/MainActivity.java
@@ -0,0 +1,40 @@
+package ua.kpi.spscs.contentproviderbrowserclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txv;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txv = findViewById(R.id.textView);
+        populateTxtWithData();
+    }
+
+    private void populateTxtWithData() {
+        String str = "";
+        String[] projection = {"id", "name"};
+        Uri uri = Uri.parse("content://com.random/users");
+        Cursor c = getContentResolver().query(uri, projection, null, null, null);
+        str += "Count: " + c.getCount() + "\n";
+        while(c.moveToNext()) {
+            str += c.getString(0) + "---";
+            String url = c.getString(1);
+            str += url + "\n";
+        }
+        txv.setText(str);
+
+    }
+
+}
